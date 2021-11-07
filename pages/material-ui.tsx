7f52c06b1d757cac187a006c5a0d954f7b3fc2fb
Shuntaro Shimizu <ut.s.shimizu@gmail.com>
2021-11-07 23:16:05 +0900

    implement drawer with material ui

--- pages/material-ui.tsx
+++ pages/material-ui.tsx
@@ -1,5 +1,17 @@
 import React from "react";
+import { Button, Drawer } from "@mui/material";
 
-const MaterialUI = () => <>Material UI</>;
+const MaterialUI: React.FunctionComponent = () => {
+  const [open, setOpen] = React.useState(false);
+
+  return (
+    <>
+      <Button onClick={() => setOpen(true)}>Open</Button>
+      <Drawer anchor="left" open={open} onClose={() => setOpen(false)}>
+        Drawer
+      </Drawer>
+    </>
+  );
+};
 
 export default MaterialUI;
