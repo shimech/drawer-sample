6ad2a22ab3da34498d9db27a422f61772be8b82d
Shuntaro Shimizu <ut.s.shimizu@gmail.com>
2021-11-10 01:14:44 +0900

    Transitionをコンポーネントに隠蔽

--- pages/react-transition-group.tsx
+++ pages/react-transition-group.tsx
@@ -2,109 +2,109 @@ import React from "react";
 import ReactDOM from "react-dom";
 import { Button } from "@mui/material";
 import { css } from "@emotion/react";
-import { Transition, TransitionStatus } from "react-transition-group";
+import { Transition } from "react-transition-group";
 
 type DrawerProps = {
   className?: string;
   duration: number;
-  mount: boolean;
-  state: TransitionStatus;
+  open: boolean;
   width: number;
   onClose?: VoidFunction;
 };
 
 const Drawer: React.FunctionComponent<DrawerProps> = (props) => {
+  const [mount, setMount] = React.useState(false);
+  const [willEnter, setWillEnter] = React.useState(true);
   const [body, setBody] = React.useState<HTMLElement>();
+
+  React.useEffect(() => {
+    if (props.open) {
+      setMount(true);
+    }
+  }, [props.open]);
 
   React.useEffect(() => {
     setBody(document.body);
   }, []);
 
-  if (body && props.mount) {
-    return ReactDOM.createPortal(
-      <div
-        className={props.className}
-        css={css`
-          inset: 0;
-          position: absolute;
-        `}
-      >
-        <div
-          className="backdrop"
-          css={[
-            css`
-              background-color: #000;
-              inset: 0;
-              opacity: 0.8;
-              position: fixed;
-              transition: opacity ${props.duration}ms;
-            `,
-            (props.state === "exiting" || props.state === "exited") &&
-              css`
-                opacity: 0;
-              `,
-          ]}
-          onClick={props.onClose}
-        />
-        <div
-          css={[
-            css`
-              background-color: #fff;
-              inset: 0;
-              height: 100vh;
-              position: absolute;
-              transform: none;
-              transition: transform ${props.duration}ms;
-              width: ${props.width}px;
-            `,
-            (props.state === "exiting" || props.state === "exited") &&
-              css`
-                transform: translateX(-${props.width}px);
-              `,
-          ]}
-          className="container"
-        >
-          {props.children}
-        </div>
-      </div>,
-      body
-    );
-  } else {
-    return null;
-  }
+  return (
+    <Transition
+      in={props.open}
+      timeout={props.duration}
+      onEnter={() => setWillEnter(true)}
+      onEntering={() => setWillEnter(false)}
+      onExited={() => setMount(false)}
+    >
+      {(state) => {
+        if (body && mount) {
+          return ReactDOM.createPortal(
+            <div
+              className={props.className}
+              css={css`
+                inset: 0;
+                position: absolute;
+              `}
+            >
+              <div
+                css={[
+                  css`
+                    background-color: #000;
+                    inset: 0;
+                    opacity: 0.8;
+                    position: fixed;
+                    transition: opacity ${props.duration}ms;
+                  `,
+                  (willEnter || state === "exiting" || state === "exited") &&
+                    css`
+                      opacity: 0;
+                    `,
+                ]}
+                onClick={props.onClose}
+              />
+              <div
+                css={[
+                  css`
+                    background-color: #fff;
+                    inset: 0;
+                    height: 100vh;
+                    position: absolute;
+                    transform: none;
+                    transition: transform ${props.duration}ms;
+                    width: ${props.width}px;
+                  `,
+                  (willEnter || state === "exiting" || state === "exited") &&
+                    css`
+                      transform: translateX(-${props.width}px);
+                    `,
+                ]}
+              >
+                {props.children}
+              </div>
+            </div>,
+            body
+          );
+        } else {
+          return null;
+        }
+      }}
+    </Transition>
+  );
 };
 
-const DURATION = 1000;
-
 const ReactTransitionGroup: React.FunctionComponent = () => {
-  const [mount, setMount] = React.useState(false);
   const [open, setOpen] = React.useState(false);
-
-  const handleExited = () => setMount(false);
 
   return (
     <>
-      <Button
-        onClick={() => {
-          setMount(true);
-          setOpen(true);
-        }}
+      <Button onClick={() => setOpen(true)}>OPEN</Button>
+      <Drawer
+        duration={1000}
+        open={open}
+        width={200}
+        onClose={() => setOpen(false)}
       >
-        OPEN
-      </Button>
-      <Transition in={open} timeout={DURATION} onExited={handleExited}>
-        {(state) => (
-          <Drawer
-            duration={DURATION}
-            mount={mount}
-            state={state}
-            width={200}
-            onClose={() => setOpen(false)}
-          >
-            Drawer
-          </Drawer>
-        )}
-      </Transition>
+        Drawer
+      </Drawer>
     </>
   );
 };
