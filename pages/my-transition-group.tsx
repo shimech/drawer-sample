37db924a1a3eb30d2fa7e90027648a5e361f6302
Shuntaro Shimizu <ut.s.shimizu@gmail.com>
2021-11-21 22:45:49 +0900

    一旦動いた

--- pages/my-transition-group.tsx
+++ pages/my-transition-group.tsx
@@ -3,135 +3,104 @@ import React from "react";
 import ReactDOM from "react-dom";
 import { Button } from "@mui/material";
 
-type TransitionContextValue = {
+type TransitionProps = {
   duration: number;
-  mount: boolean;
   open: boolean;
-  willEnter: boolean;
+  children: (mount: boolean, willEnter: boolean) => React.ReactElement | null;
 };
 
-const TransitionContext = React.createContext<TransitionContextValue>({
-  duration: 0,
-  mount: false,
-  open: false,
-  willEnter: true,
-});
-
-type TransitionProps = Omit<TransitionContextValue, "mount" | "willEnter"> & {};
-
-const Transition: React.FunctionComponent<TransitionProps> = (props) => {
-  const [value, setValue] = React.useState<TransitionContextValue>({
-    duration: props.duration,
-    mount: false,
-    open: props.open,
-    willEnter: true,
-  });
+const Transition = (props: TransitionProps) => {
+  const [mount, setMount] = React.useState(false);
+  const [willEnter, setWillEnter] = React.useState(true);
 
   React.useEffect(() => {
     let timeout: NodeJS.Timeout;
-    setValue((prevValue) => ({ ...prevValue, open: props.open }));
     if (props.open) {
-      setValue((prevValue) => ({ ...prevValue, mount: true, willEnter: true }));
+      setWillEnter(true);
+      setMount(true);
     } else {
-      timeout = setTimeout(
-        () => setValue((prevValue) => ({ ...prevValue, mount: false })),
-        value.duration
-      );
+      timeout = setTimeout(() => setMount(false), props.duration);
     }
 
     return () => clearTimeout(timeout);
-  }, [props.open, value.duration]);
+  }, [props.open, props.duration]);
 
   React.useEffect(() => {
-    if (value.mount) {
-      setValue((prevValue) => ({ ...prevValue, willEnter: false }));
+    if (mount) {
+      setWillEnter(false);
     }
-  }, [value.mount]);
+  }, [mount]);
 
-  React.useEffect(() => {
-    setValue((prevValue) => ({ ...prevValue, duration: props.duration }));
-  }, [props.duration]);
-
-  return (
-    <TransitionContext.Provider value={value}>
-      {props.children}
-    </TransitionContext.Provider>
-  );
+  return props.children(mount, willEnter);
 };
 
-type DrawerContentProps = {
+type DrawerProps = Omit<TransitionProps, "children"> & {
   width: number;
   onClose?: VoidFunction;
 };
 
-const DrawerContent: React.FunctionComponent<DrawerContentProps> = (props) => {
+const Drawer: React.FunctionComponent<DrawerProps> = (props) => {
   const [body, setBody] = React.useState<HTMLElement>();
-  const value = React.useContext(TransitionContext);
-
   React.useEffect(() => {
     setBody(document.body);
   }, []);
 
-  if (body && value.mount) {
-    return ReactDOM.createPortal(
-      <div
-        css={css`
-          inset: 0;
-          position: absolute;
-        `}
-      >
-        <div
-          css={[
-            css`
-              background-color: #000;
-              inset: 0;
-              opacity: 0.8;
-              position: fixed;
-              transition: opacity ${value.duration}ms;
-            `,
-            (!value.open || value.willEnter) &&
-              css`
-                opacity: 0;
-              `,
-          ]}
-          onClick={props.onClose}
-        />
-        <div
-          css={[
-            css`
-              background-color: #fff;
-              inset: 0;
-              height: 100vh;
-              position: absolute;
-              transform: none;
-              transition: transform ${value.duration}ms;
-              width: ${props.width}px;
-            `,
-            (!value.open || value.willEnter) &&
-              css`
-                transform: translateX(-${props.width}px);
-              `,
-          ]}
-        >
-          {props.children}
-        </div>
-      </div>,
-      body
-    );
-  } else {
-    return null;
-  }
+  return (
+    <Transition duration={props.duration} open={props.open}>
+      {(mount, willEnter) => {
+        if (body && mount) {
+          return ReactDOM.createPortal(
+            <div
+              css={css`
+                inset: 0;
+                position: absolute;
+              `}
+            >
+              <div
+                css={[
+                  css`
+                    background-color: #000;
+                    inset: 0;
+                    opacity: 0.8;
+                    position: fixed;
+                    transition: opacity ${props.duration}ms;
+                  `,
+                  (!props.open || willEnter) &&
+                    css`
+                      opacity: 0;
+                    `,
+                ]}
+                onClick={props.onClose}
+              />
+              <div
+                css={[
+                  css`
+                    background-color: #fff;
+                    inset: 0;
+                    height: 100vh;
+                    position: absolute;
+                    transform: none;
+                    transition: transform ${props.duration}ms;
+                    width: ${props.width}px;
+                  `,
+                  (!props.open || willEnter) &&
+                    css`
+                      transform: translateX(-${props.width}px);
+                    `,
+                ]}
+              >
+                {props.children}
+              </div>
+            </div>,
+            body
+          );
+        } else {
+          return null;
+        }
+      }}
+    </Transition>
+  );
 };
-
-type DrawerProps = TransitionProps & DrawerContentProps;
-
-const Drawer: React.FunctionComponent<DrawerProps> = (props) => (
-  <Transition duration={props.duration} open={props.open}>
-    <DrawerContent width={props.width} onClose={props.onClose}>
-      {props.children}
-    </DrawerContent>
-  </Transition>
-);
 
 const MyTransitionGroup: React.FunctionComponent = () => {
   const [open, setOpen] = React.useState(false);
